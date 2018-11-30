69895eadbbd95f790de6e9540db6f5a09ec3a3a8
Jiyoung Lim <33502257+sam-lim@users.noreply.github.com>
2018-11-30 18:27:33 -0500

    Filter by year
    Filter by year works

--- main.js
+++ main.js
@@ -5,6 +5,8 @@
 // even though it is defined on line 8.
 window.onload = start;
 
+
+
 // This is where all of our javascript code resides. This method
 // is called by "window" when the document (everything you see on
 // the screen) has finished loading.
@@ -13,7 +15,7 @@ function start() {
     var width = 900;
     var height = 500;
 
-
+    // Initial graph -> default year = 2010
     d3.csv("movies.csv", function (csv) {
         for (var i = 0; i < csv.length; ++i) {
             csv[i].gross  = Number(csv[i].gross);
@@ -45,25 +47,19 @@ function start() {
             .attr("width", width)
             .attr("height", height);
 
-        // Where details of movie displays
-        var chart2 = d3.select("#chart2")
-            .append("svg:svg")
-            .attr("width", width)
-            .attr("height", height);
-
-
         // Dots
         var temp1 = chart1.selectAll("circle")
             .data(csv)
             .enter()
             .append("circle")
+            .attr('class', 'dot')
             .attr("id", function (d, i) {
                 return i;
             })
             .attr("stroke", "black")
-            //.attr("fill", "pink")
-            .filter(function (d) {return d.title_year == 2012 }) //170051787
-            .attr("class", "moreGross")
+            .attr("fill", "pink")
+            .filter(function (d) {return d.title_year == 2010 }) //170051787
+            //.attr("class", "moreGross")
             .attr("cx", function (d) {
                 return xScale(d.imdb_score);
             })
@@ -80,15 +76,7 @@ function start() {
                 document.getElementById("budget").textContent = "$ "+d.budget;
                 document.getElementById("gross").textContent =  "$ "+d.gross;
                 document.getElementById("genre").textContent = d.genres;
-                
-                // Code from P4 brusing - disregard it. I just kept in case if we want to do brushing
-                d3.selectAll(".extent").style("display", "none");
-                d3.selectAll(".resize").style("display", "none");
-                chart1.selectAll("circle").classed("selected", false);
-                chart2.selectAll("circle").classed("selected2", false);
-                chart2.selectAll("circle").classed("selected", function (d, k) {
-                    return i === k;
-                });
+            
             });
 
     
@@ -118,5 +106,111 @@ function start() {
             .text("Budget");
      
     });
+
+
+
+    // Dropdown for years
+    d3.select("select")
+        .on("change", function (d) {
+            var selected = d3.select("#dropdown").node().value;
+            console.log(selected);
+
+            d3.selectAll(".dot").remove();
+            d3.select("svg").remove();
+
+            d3.csv("movies.csv", function (csv) {
+                for (var i = 0; i < csv.length; ++i) {
+                    csv[i].gross  = Number(csv[i].gross);
+                    csv[i].budget  = Number(csv[i].budget);
+                    csv[i].imdb_score  = Number(csv[i].imdb_score);
+                    csv[i].movie_title  = String(csv[i].movie_title);
+                    csv[i].title_year  = String(csv[i].title_year);
+                    csv[i].movie_imdb_link  = String(csv[i].movie_imdb_link);
+                }
+                var grossExtent = d3.extent(csv, function (row) { return row.gross; });
+                var budgetExtent = d3.extent(csv, function (row) { return row.budget; });
+
+                var xScale = d3.scale.linear().domain([0, 10]).range([80, 870]);
+                var yScale = d3.scale.linear().domain([0,263700000]).range([470, 30]); //[0,263700000]
+                var rScale = d3.scale.linear().domain(grossExtent).range([3, 60]);
+
+
+                var xAxis = d3.svg.axis().scale(xScale);
+                var yAxis = d3.svg.axis().scale(yScale);
+                yAxis.orient("left");
+
+                // Dot plot
+                var chart1 = d3.select("#chart1")
+                .append("svg:svg")
+                .attr("width", width)
+                .attr("height", height);
+
+                // Dots
+                var temp1 = chart1.selectAll("circle")
+                    .data(csv)
+                    .enter()
+                    .append("circle")
+                    .attr("id", function (d, i) {
+                        return i;
+                    })
+                    .attr("stroke", "black")
+                    .attr("fill", "pink")
+                    .filter(function (d) {return d.title_year == selected }) //170051787
+                    //.attr("class", "moreGross")
+                    .attr("cx", function (d) {
+                        return xScale(d.imdb_score);
+                    })
+                    .attr("cy", function (d) {
+                        return yScale(d.budget);
+                    })
+                    .attr("r", function(d) {
+                        return rScale(d.gross);
+                    })
+                    .on("click", function (d, i) {
+                        document.getElementById("title").textContent = d.movie_title;
+                        document.getElementById("director").textContent = d.director_name;
+                        document.getElementById("IMDd").textContent = d.imdb_score;
+                        document.getElementById("budget").textContent = "$ "+d.budget;
+                        document.getElementById("gross").textContent =  "$ "+d.gross;
+                        document.getElementById("genre").textContent = d.genres;
+                    });
+        
+            
+                // x-axis
+                chart1 // or something else that selects the SVG element in your visualizations
+                    .append("g") // create a group node
+                    .attr("transform", "translate(0," + (height - 30) + ")")
+                    .call(xAxis) // call the axis generator
+                    .append("text")
+                    .attr("class", "label")
+                    .attr("x", width - 16)
+                    .attr("y", -6)
+                    .style("text-anchor", "end")
+                    .text("IMDb score");
+                
+                // y-axis
+                chart1 // or something else that selects the SVG element in your visualizations
+                    .append("g") // create a group node
+                    .attr("transform", "translate(80, 0)")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("class", "label")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", ".71em")
+                    .style("text-anchor", "end")
+                    .text("Budget");
+             
+            });
+    })
+
+
+    d3.select("select")
+        .append('p')
+        .append('button')
+        .text('Filter Data')
+        .on('click', function () {
+            
+        });
 
 }
